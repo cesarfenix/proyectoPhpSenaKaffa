80cca5cdba3ebe6bfbf5b2ba5c8b628772cc8bed
cesaresmas.au@gmail.com <cesaresmas.au@gmail.com>
2021-06-29 18:24:15 -0500

    proyecto ok

--- home/index.php
+++ home/index.php
@@ -3,7 +3,7 @@
 	if (isset($_SESSION["usuario"])) {
 		require_once "../view/partials/headerAdmin.php";
 		require_once "../view/home/index.php";
-		//require_once "../view/partials/footer.php";
+		require_once "../view/partials/footer.php";
 	}
 
  ?>
--- view/usuarios/contactanos.php
+++ view/usuarios/contactanos.php
@@ -5,11 +5,43 @@
     <!--<link href="../home/kaffa.css" type="text/css" rel="stylesheet" media />-->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet" />
     <link href="../home/kaffa.css" type="text/css" rel="stylesheet" media />
+    <link href="contactanos.css" type="text/css" rel="stylesheet" media />
     <title>Café de origen Colombiano - Kaffa specialty coffe</title>
     <link rel="icon" href="../../imagenes/logoMarron.png" />
-    include("common-style.php");
-  </head>
-<body>
+
+   <!-- include("common-style.php");-->
+    </head>
+   <body>
+      <div class="principal">
+         <div class="principal-1">
+         <h1>Café de origen Colombiano - Kaffa specialty coffe</h1><br>
+            <p>Encuentra Todo lo que necesitas desde la seguridad y comodidad de tu hogar</p>
+      <div class=" contenedor-contacto">
+         <!--<figure>
+         <img src="../../imagenes/4.jpg"alt="">
+         <figcaption> texto</figcaption>
+      </figure>-->
+       <form action="">
+        <div class="mb-1">
+           <input type="text" id="nombre" placeholder="Nombres">
+        </div>
+        <div class="mb-1">
+           <input type="text" id="apellidos" placeholder="Apellidos"><br>
+        </div>
+        <div class="mb-1">
+           <input type="email" id="email" placeholder="Email">
+       </div>
+       <div class="mb-1">
+        <textarea class="form-control" rows="4" id="mensaje" placeholder="Mensaje" onresize="width"></textarea>
+       </div>
+       <a href="#" class="btn btn-primary" role="button" data-bs-toggle="button">Enviar</a>
+
+       </form>
+       </div>
+   </div>
+   </div>
+ 
+
 	
 </body>
 </html>
